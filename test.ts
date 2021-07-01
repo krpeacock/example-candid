903c4d1c70a50770fd136980e24232865ce40253
Kyle Peacock <kylpeacock@gmail.com>
2021-07-01 11:04:30 -0700

    nothing

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { Identity } from "./output";
-
-const actor = new Identity();
-
-actor.lookup("10015").then((value) => {
-  value.forEach((deviceData) => {
-    console.log(deviceData.alias);
-  });
-});
